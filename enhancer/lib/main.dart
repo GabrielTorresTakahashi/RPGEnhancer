03d49fc09ef3e2aa8ea0fd81e0cafed8a836fea7
GabrielTorresTakahashi <600601@univem.edu.br>
2022-04-28 19:22:38 -0300

    ok

---
+++ enhancer/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:enhancer/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Enhancer());
+}
+
+
+class Enhancer extends StatelessWidget {
+  const Enhancer({ Key? key }) : super(key: key);
+
+  //cores
+  static const Color primary = Color(0xFF81171b);
+  static const Color background = Color(0xFF540804);
+  //cores
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const HomeScreen(),
+
+      theme: ThemeData(
+
+        backgroundColor: background,
+
+
+
+
+        appBarTheme: AppBarTheme(
+          backgroundColor: primary
+        ),
+      ),
+    );
+  }
+}
