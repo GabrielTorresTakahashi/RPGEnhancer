7fa0285b8b0bb5cb9618235582f2d619716ffe63
GabrielTorresTakahashi <600601@univem.edu.br>
2022-05-04 11:42:29 -0300

    ok

--- enhancer/lib/screens/loot_generator.dart
+++ enhancer/lib/screens/loot_generator.dart
@@ -7,22 +7,109 @@ class LootGenerator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).backgroundColor,
-      body: const LootTypeSelector(),
+        backgroundColor: Theme.of(context).backgroundColor,
+        appBar: AppBar(backgroundColor: Theme.of(context).primaryColor),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            LootTypeSelector(),
+          ],
+        ));
+  }
+}
+
+class GeneratedLoot extends StatelessWidget {
+  const GeneratedLoot({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+//Junção do LevelAmount e do TypeCheckbox, montando o layout de seleção de tipo
+// ignore: must_be_immutable
+class LootTypeSelector extends StatelessWidget {
+  bool currency = false;
+  bool scrolls = false;
+  bool magicItems = false;
+  bool normalItems = false;
+  String level = '';
+  String ammount = '';
+
+  LootTypeSelector({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            DropdownSelector(
+              options: const ['-Nível-', '1-4', '5-10', '11-17', '17+'],
+              controls: level,
+            ),
+            DropdownSelector(
+              options: const ['-Quantia-', 'Individual', 'Pilha'],
+              controls: ammount,
+            )
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextCheckbox(
+                  text: 'Currency',
+                  controls: currency,
+                ),
+                TextCheckbox(
+                  text: 'Scrolls',
+                  controls: scrolls,
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextCheckbox(
+                  text: 'Magic Items',
+                  controls: magicItems,
+                ),
+                TextCheckbox(
+                  text: 'Normal Items',
+                  controls: normalItems,
+                ),
+              ],
+            ),
+          ],
+        ),
+        GeneratedLoot(),
+      ],
     );
   }
 }
 
-class LevelAmountSelector extends StatefulWidget {
+//Dropdown que recebe um vetor de string com as opções.
+//Usado para selecionar o nível e quantia do tesouro
+// ignore: must_be_immutable
+class DropdownSelector extends StatefulWidget {
   final List<String> options;
+  String controls;
 
-  const LevelAmountSelector(this.options, {Key? key}) : super(key: key);
+  DropdownSelector({Key? key, required this.options, required this.controls})
+      : super(key: key);
 
   @override
-  State<LevelAmountSelector> createState() => _LevelAmountSelectorState();
+  State<DropdownSelector> createState() => _DropdownSelectorState();
 }
 
-class _LevelAmountSelectorState extends State<LevelAmountSelector> {
+class _DropdownSelectorState extends State<DropdownSelector> {
   late String currentValue = widget.options[0];
   @override
   Widget build(BuildContext context) {
@@ -37,6 +124,8 @@ class _LevelAmountSelectorState extends State<LevelAmountSelector> {
           onChanged: (String? newValue) {
             setState(() {
               currentValue = newValue!;
+              widget.controls = currentValue;
+              debugPrint('Selected ' + widget.controls);
             });
           },
           items: widget.options.map<DropdownMenuItem<String>>((String value) {
@@ -55,15 +144,20 @@ class _LevelAmountSelectorState extends State<LevelAmountSelector> {
   }
 }
 
-class TypeCheckbox extends StatefulWidget {
+//Checkbox com texto ao lado, recebe por parâmetro a string do texto
+//Usado para selecionar o tipo de tesouro
+// ignore: must_be_immutable
+class TextCheckbox extends StatefulWidget {
   final String text;
-  const TypeCheckbox(this.text, {Key? key}) : super(key: key);
+  bool controls;
+  TextCheckbox({Key? key, required this.text, required this.controls})
+      : super(key: key);
 
   @override
-  State<TypeCheckbox> createState() => _TypeCheckboxState();
+  State<TextCheckbox> createState() => _TextCheckboxState();
 }
 
-class _TypeCheckboxState extends State<TypeCheckbox> {
+class _TextCheckboxState extends State<TextCheckbox> {
   bool isChecked = false;
   @override
   Widget build(BuildContext context) {
@@ -79,50 +173,12 @@ class _TypeCheckboxState extends State<TypeCheckbox> {
           onChanged: (bool? value) {
             setState(() {
               isChecked = value!;
+              widget.controls = isChecked;
+              debugPrint(widget.text + ' ' + widget.controls.toString());
             });
           },
         ),
         Text(widget.text, style: checkboxText)
-      ],
-    );
-  }
-}
-
-class LootTypeSelector extends StatelessWidget {
-  const LootTypeSelector({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: const [
-            LevelAmountSelector(['-Nível-']),
-            LevelAmountSelector(['-Quantia-'])
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                TypeCheckbox('Dinheiro'),
-                TypeCheckbox('Pergaminhos'),
-              ],
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                TypeCheckbox('Itens Normais'),
-                TypeCheckbox('Itens Mágicos'),
-              ],
-            ),
-          ],
-        ),
       ],
     );
   }
