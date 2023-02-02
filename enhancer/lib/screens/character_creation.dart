90263ce8128de6c8e3c8c69549287fd5a7b9f037
Gabriel Torres Takahashi <gabrielspot1234@gmail.com>
2023-02-01 22:32:13 -0300

    Added race dropdown to characte creation srceen

--- enhancer/lib/screens/character_creation.dart
+++ enhancer/lib/screens/character_creation.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:enhancer/database/model/race_model.dart';
 import 'package:enhancer/database/services/database_helper.dart';
 import 'package:enhancer/settings/color_settings.dart';
@@ -23,24 +25,58 @@ class _CharacterCreationScreenState extends State<CharacterCreationScreen> {
       appBar: AppBar(),
       body: FormBuilder(
         key: _formKey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            FormBuilderTextField(
-                style: listTileText,
-                decoration: textInputDecoration.copyWith(hintText: 'Nome'),
-                name: 'name'),
-            FutureBuilder<List<Race>?>(
-              future: DatabaseHelper.getAllRaces(),
-              builder: (context, snapshot) {
-                if (snapshot.hasData && snapshot.data != null) {
-                  snapshot.data!;
-                }
-                return const Text('No Races Found');
-              },
-            ),
-            const SizedBox(height: 10)
-          ],
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              FormBuilderTextField(
+                  style: listTileText,
+                  decoration: textInputDecoration.copyWith(hintText: 'Nome'),
+                  name: 'name'),
+              FutureBuilder<List<Race>?>(
+                future: DatabaseHelper.getAllRaces(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData && snapshot.data != null) {
+                    return FormBuilderDropdown(
+                        initialValue: null,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Theme.of(context).colorScheme.primary,
+                          labelText: '-Raça-',
+                          labelStyle: dropdownText,
+                          suffixIconColor:
+                              Theme.of(context).colorScheme.onPrimary,
+                          suffixIcon: const Icon(Icons.arrow_drop_down),
+                        ),
+                        icon: const SizedBox(),
+                        dropdownColor: Colors.blue,
+                        style: dropdownText,
+                        name: 'raceId',
+                        items: snapshot.data!
+                            .map(
+                              (race) => DropdownMenuItem(
+                                value: race.id.toString(),
+                                child: Text(race.name),
+                              ),
+                            )
+                            .toList());
+                  }
+                  return const Text('No Races Found');
+                },
+              ),
+              FormBuilderTextField(name: 'level'),
+              ElevatedButton(
+                onPressed: () {
+                  _formKey.currentState!.save();
+                  log(_formKey.currentState!.value.toString());
+                },
+                child: const Text('SUBMIT'),
+              ),
+              const SizedBox(height: 10),
+            ],
+          ),
         ),
       ),
     );
